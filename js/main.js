4e373e8a387201861ae9c1367b19bd2159a0537a
santiago ruiz <santiagoruiz9416@gmail.com>
2022-06-02 01:42:25 -0300

    Ya hice dos ejemplos usando while y for

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
+/*
+//Ejercicio para sumar de a uno usando while
+let valor = parseInt(prompt("Ingrese un numero de 1 al 10"));
 
-let nombre = prompt("Ingresa un nombre")
+while (valor <= 10 && valor !== "") {
+    valor++;
+    alert(valor);
+
+}
+*/
+
+
+
+/*
+//Ejercicio con for para sacar el IVA
+
+for(let i = 1; i < 4; i++){
+    let monto = parseFloat(prompt("Ingresa hasta 3 veces el monto que desees sumarle el IVA"))
+    let Iva = 1.21
+    let masIva = monto * Iva
+    alert("El Subtotal del valor N°" + i + " ingresado es  $" + monto + "\n El total incluyendo el IVA es " + masIva);
+}
+*/

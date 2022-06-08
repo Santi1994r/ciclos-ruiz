bd82a6ace4656dd73e1d1e378d447e5186966e95
santiago ruiz <santiagoruiz9416@gmail.com>
2022-06-07 23:28:00 -0300

    creo el simulador de costo en cuotas

--- js/main.js
+++ js/main.js
@@ -1,23 +1,164 @@
-/*
-//Ejercicio para sumar de a uno usando while
-let valor = parseInt(prompt("Ingrese un numero de 1 al 10"));
+//SIMULADOR DE VALOR POR CUOTA Y TOTAL
 
-while (valor <= 10 && valor !== "") {
-    valor++;
-    alert(valor);
+let resultado = 0;
+
+
+function intCuota(valor){
+    let conInteres = valor * 0.1;
+    resultado = valor + conInteres
+    return resultado;
+}
+
+function calCuota(resultado,cuotas){
+    resultadoFinal = resultado / cuotas;
+    return resultadoFinal;
+}
+
+function dictaTotal (){
+    alert("Al pagar en "+ cuotas + " cuotas se le suma un interes del 10%. \n Su total a pagar es de $" + resultado + "\n Cada cuota va a tener un valor de $" + resultadoFinal);
 
 }
-*/
 
-
-
-/*
-//Ejercicio con for para sacar el IVA
-
-for(let i = 1; i < 4; i++){
-    let monto = parseFloat(prompt("Ingresa hasta 3 veces el monto que desees sumarle el IVA"))
-    let Iva = 1.21
-    let masIva = monto * Iva
-    alert("El Subtotal del valor N°" + i + " ingresado es  $" + monto + "\n El total incluyendo el IVA es " + masIva);
+function calculo(){
+    if(cuotas === 1){
+        calCuota(valor,cuotas);
+        alert("Al pagarlo en una sola cuota, esta no tiene recargo. Su monto a pagar tiene un total de  $" + valor);
+    }else if(cuotas === 2){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal ();    
+    }else if(cuotas === 3){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal ();
+    }else if(cuotas === 4){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal ();
+    }else if(cuotas === 5){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal ();
+    }else if(cuotas === 6){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal ();
+    }else if(cuotas === 7){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal ();
+    }else if(cuotas === 8){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal ();
+    }else if(cuotas === 9){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal ();
+    }else if(cuotas === 10){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal ();
+    }else if(cuotas === 11){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal ();
+    }else if(cuotas === 12){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal ();
+    }else{
+        alert("La cantidad de cuotas elegida no es correcta ")
+    }
 }
-*/
+
+function calculo2(){
+    if(cuotas === 1){
+        calCuota(valor,cuotas);
+        alert("Al pagarlo en una sola cuota, esta no tiene recargo. Su monto a pagar tiene un total de  $" + valor);
+    }else if(cuotas === 2){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal();    
+    }else if(cuotas === 3){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal();
+    }else if(cuotas === 4){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal();
+    }else if(cuotas === 5){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal();
+    }else if(cuotas === 6){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal();
+    }else if(cuotas === 7){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal();
+    }else if(cuotas === 8){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal();
+    }else if(cuotas === 9){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal();
+    }else if(cuotas === 10){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal();
+    }else if(cuotas === 11){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal();
+    }else if(cuotas === 12){
+        intCuota(valor);
+        calCuota(resultado,cuotas);
+        dictaTotal();
+    }else if(cuotas === "no"){
+        alert("Saludos, vuelva pronto");
+    }else if(cuotas === "si"){
+        valor = parseFloat(prompt("Ingrese el valor a calcular en cuotas"));
+        cuotas = parseInt(prompt("Ingrese en cuantas cuotas lo quiere pagar \n Solo tiene hasta 12 cuotas"));
+        calculo();
+    }else{
+        alert("La respuesta no es correcta");
+    }
+}
+//funcion para volver a preguntar si quiere seguir calculando
+function otraVez(){
+    cuotas = prompt("¿Quiere seguir calculando? \n -si \n -no");
+}
+
+function otraVezWhile() {
+//nose como hacer para que me siga preguntando todo el tiempo si quiero seguir calculando.
+    //-- --  --  --  --  --  --  --  ---  ---  ---  ---  --- --- --- -
+    while(cuotas !== "no" || cuotas !== "NO"){
+        
+        if(cuotas === "si" || cuotas === "SI"){
+            calculo2(); 
+        }else if(cuotas === ""){
+            alert("Opcion incorrecta");
+            break;
+        }else{
+            alert("saludos, vuelva pronto");
+            break;
+        }
+    }
+    
+}
+
+
+
+
+//COMIENZO DE PROGRAMA
+let valor = parseFloat(prompt("Ingrese el valor a calcular en cuotas"));
+let cuotas = parseInt(prompt("Ingrese en cuantas cuotas lo quiere pagar \n Solo tiene hasta 12 cuotas"));
+calculo();
+otraVez();
+otraVezWhile();
